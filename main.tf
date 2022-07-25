be682d2018aa9567aea9084fe153fd721ed859e6
JamesHashicorp <jallen@hashicorp.com>
2022-07-25 10:54:33 +0100

    typo fix for the org name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "JamesHashicorp "
+    organization = "JamesHashicorp"
 
     workspaces {
       name = "GitHub-Actions-Workspace"
