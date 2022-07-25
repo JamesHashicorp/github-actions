61b4c75977cc6dc66b4c033e17405983d20c27f6
JamesHashicorp <jallen@hashicorp.com>
2022-07-25 10:53:43 +0100

    Reset the backend

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "JamesHashicorp "
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "GitHub-Actions-Workspace"
     }
   }
 }
